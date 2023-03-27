1c935ed7d96e7ce6591ef8be0fc507a8f65ca691
mkiel01 <mkielkowski01@gmail.com>
2023-03-27 18:42:22 +0200

    first commit

---
+++ Easy/move-zeroes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        counter = 0
+
+
+        for i in nums:
+            if i == 0:
+                counter += 1
+
+        while counter > 0:
+    
+
+            nums.remove(0)
+            counter -= 1
+            nums.append(0)
+        return nums
